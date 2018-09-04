4535ccdc8b7ff98315d6815d4370bb9c704865a2
Xander <annecy169@gmail.com>
2018-09-04 12:36:40 +0100

    Converting to Node app

--- app.js
+++ app.js
@@ -1,11 +1,46 @@
-var http = require('http');
-const fs = require('fs');
+var express = require('express');
+var app = express();
+app.set('view engine', 'ejs');
 
-var server = http.createServer(function(req, res) {
-    res.statusCode = 200;
-    res.setHeader('Content-type', 'text/html');
-    var html = fs.readFileSync('./app/html/DashBoard.html');
-    res.write(html);    
-    res.end();
+var Users = {
+
+  'Xander': {
+    age: 19,
+    occupation: 'Developer',
+    hobby: 'Driving'
+  },
+
+  'Adrian': {
+    age: 34,
+    occupation: 'Senior Solutions Architect',
+    hobby: 'Running'
+  }
+};
+
+app.use(express.static(__dirname + '/app'));
+
+app.get('/', function (req, res) {
+  res.send('Main page is up and running!');
 });
-server.listen(8080);
+
+app.get('/Dashboard', function (req, res) {
+    res.render('Dashboard');
+});
+
+app.get('/Team', function (req, res) {
+    res.render('Team');
+});
+
+app.get('/user', function (req, res) {
+    var check = Users[req.query.name];
+    if (check) {
+        res.render('User', { name: req.query.name, info: check });
+    } else {
+        res.send('User does not exist...');
+    }
+});
+
+
+app.listen(3000, function () {
+  console.log('server started on port 3000');
+});
